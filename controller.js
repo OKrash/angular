8d491293a74c7429da88461bbd7d0198b270094d
unknown <oskar2508@hotmail.com>
2018-11-21 23:11:23 -0500

    Lista de Peronas

--- controller.js
+++ controller.js
@@ -1,5 +1,21 @@
-'use strict';
-angular.module("MiPrimeraApp",[]);
-.controller("PrimerControlador",function($scope){
+var app=angular.module("MiPrimeraApp",[]);
+app.controller("PrimerControlador",function($scope){
 	$scope.nombre="Oscar";
+	$scope.nuevaPersona={};
+	$scope.personas =[
+		{
+			nombre:"Juan",
+			apellido:"Perez",
+			edad :12
+		},
+		{
+			nombre:"Pedro",
+			apellido:"Leyton",
+			edad:15
+		}
+	];
+	$scope.agregarPersona = function(){
+		$scope.personas.push($scope.nuevaPersona);
+		$scope.nuevaPersona={};
+	}
 });
